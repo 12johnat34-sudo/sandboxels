429e484c671c6172120b86239568e2b7d1de1866
12johnat34-sudo <12johnat34@gmail.com>
2025-08-04 01:13:22 -0500

    Create boompaste.js

---
+++ mods/boompaste.js
@@ -0,0 +1,17 @@
+color: "#008009",
+  behavior: behaviors.wall,
+  burn:300%,
+  cartegory: "weapons",
+  state: "solid",
+}
+  
+
+elements.boompaste = {
+	color: "#008000",
+	behavior: behaviors.STURDYPOWDER,
+	category: "weapons",
+	state: "solid",
+  reactions: {
+    "water": { elem1:"activated-b-p" , elem2: null },
+  }
+}
